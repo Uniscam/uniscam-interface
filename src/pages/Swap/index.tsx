f12991128b82f751a04d29e0e30b785f8f81b7bf
神樹桜乃 <kodama@sakuno.moe>
2020-10-11 16:24:42 +0800

    Fix

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -163,7 +163,7 @@ export default function Swap() {
   const noRoute = !route
 
   // check whether the user has approved the router on the input token
-  const [approval, approveCallback] = useApproveCallbackFromTrade(trade, allowedSlippage, chainId)
+  const [approval, approveCallback] = useApproveCallbackFromTrade(trade, allowedSlippage)
 
   // check if user has gone through approval process, used to show two step buttons, reset on token change
   const [approvalSubmitted, setApprovalSubmitted] = useState<boolean>(false)
