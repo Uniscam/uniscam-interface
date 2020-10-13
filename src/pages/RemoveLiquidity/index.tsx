ca6d94686b60082ca96ee0ed0bff1663c8d793f9
神樹桜乃 <kodama@sakuno.moe>
2020-10-14 00:15:46 +0800

    Target new lp token name

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' }
     ]
     const domain = {
-      name: 'Uniswap V2',
+      name: 'Bestswap LP Token',
       version: '1',
       chainId: chainId,
       verifyingContract: pair.liquidityToken.address
