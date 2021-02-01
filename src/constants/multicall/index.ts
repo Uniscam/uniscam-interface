819831467a6866fe87a87183fc2dc210f6e672ce
Frank Wei <frank@frankwei.xyz>
2021-02-01 16:48:04 +0800

    feat: be ready for OKChain Testnet

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -11,6 +11,8 @@ const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.BSC_TESTNET]: '0xe348b292e8eA5FAB54340656f3D374b259D658b8',
   [ChainId.HECO_MAINNET]: '0x56171094a15B8caC4314C0F8930100B939503bd9',
   [ChainId.HECO_TESTNET]: '0xD1dbc88E6a94053B11dA07CA3E5CE24B282d3d32',
+  // @XXX: Deployed by Frank, ready when uniscam-sdk have ChainId of OKChain Testnet
+  // [ChainId.OKCHAIN_TESTNET]: '0xbed5513c2fd4eae41bad3aff77ec9ef372aaf578',
   [ChainId.MATIC_MAINNET]: '0x95028E5B8a734bb7E2071F96De89BABe75be9C8E'
 }
 
