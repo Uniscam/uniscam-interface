3577f374e2d32ac78a6d6ed449f39ed80913f7c7
神樹桜乃 <kodama@sakuno.moe>
2021-02-23 12:14:06 +0800

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Unisave Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://unpkg.com/@lychees/default-token-list@1.1.7/build/uniscam-default.tokenlist.json'
+  'https://unpkg.com/@lychees/default-token-list@1.1.8/build/uniscam-default.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
