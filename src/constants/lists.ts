f758f13193a8bfc506bcabf23bf484b637752e20
神樹桜乃 <kodama@sakuno.moe>
2021-04-06 16:01:24 +0800

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,4 +2,7 @@
 export const DEFAULT_TOKEN_LIST_URL =
   'https://unpkg.com/@lychees/default-token-list@1.1.8/build/uniscam-default.tokenlist.json'
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  "https://unpkg.com/@lychees/matataki-token-list@1.0.0/build/unisave-matataki.tokenlist.json"
+]
