59cf8260a6de12a175ee0cc15f62732cb4fa3ce6
神樹桜乃 <kodama@sakuno.moe>
2021-04-06 16:37:24 +0800

    Update getTokenList.ts

--- src/utils/getTokenList.ts
+++ src/utils/getTokenList.ts
@@ -1,11 +1,11 @@
 import { TokenList } from '@uniswap/token-lists'
-import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
-import Ajv from 'ajv'
+// import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
+// import Ajv from 'ajv'
 import contenthashToUri from './contenthashToUri'
 import { parseENSAddress } from './parseENSAddress'
 import uriToHttp from './uriToHttp'
 
-const tokenListValidator = new Ajv({ allErrors: true }).compile(schema)
+// const tokenListValidator = new Ajv({ allErrors: true }).compile(schema)
 
 /**
  * Contains the logic for resolving a list URL to a validated token list
@@ -55,14 +55,14 @@ export default async function getTokenList(
     }
 
     const json = await response.json()
-    if (!tokenListValidator(json)) {
-      const validationErrors: string =
-        tokenListValidator.errors?.reduce<string>((memo, error) => {
-          const add = `${error.dataPath} ${error.message ?? ''}`
-          return memo.length > 0 ? `${memo}; ${add}` : `${add}`
-        }, '') ?? 'unknown error'
-      throw new Error(`Token list failed validation: ${validationErrors}`)
-    }
+    // if (!tokenListValidator(json)) {
+    //   const validationErrors: string =
+    //     tokenListValidator.errors?.reduce<string>((memo, error) => {
+    //       const add = `${error.dataPath} ${error.message ?? ''}`
+    //       return memo.length > 0 ? `${memo}; ${add}` : `${add}`
+    //     }, '') ?? 'unknown error'
+    //   throw new Error(`Token list failed validation: ${validationErrors}`)
+    // }
     return json
   }
   throw new Error('Unrecognized list URL protocol.')
